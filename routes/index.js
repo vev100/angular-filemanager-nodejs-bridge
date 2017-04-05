10210d6d1277e7748b121bbc2d5ecf9ad0c88bf5
Furqan Aziz <furqan.aziz@arbisoft.com>
2017-04-05 22:18:54 +0500

    Fixed all issues with respect to 1.5.1

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var pathResolver = require('../utils/pathresolver');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.body.destination)));
+    cb(null, path.join(pathResolver.baseDir(req), req.body.destination));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
@@ -24,8 +24,9 @@ routes.post('/list', function (req, res, next) {
 
   var promise;
   var self = this;
-  var fsPath = path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.body.path));
+  var fsPath = path.join(pathResolver.baseDir(req), req.body.path);
 
+  console.log(fsPath);
   promise = fs.statAsync(fsPath).then(function(stats) {
     if(!stats.isDirectory()) {
       throw new Error("Directory " + fsPath + ' does not exist!');
@@ -40,14 +41,13 @@ routes.post('/list', function (req, res, next) {
 
     return Promise.map(fileNames, function(fileName) {
 
-      var filePath = path.join(fsPath, pathResolver.pathGuard(fileName));
+      var filePath = path.join(fsPath, fileName);
 
       return fs.statAsync(filePath).then(function(stat) {
 
         return {
           name: fileName,
-          // rights: "Not Implemented", // TODO
-          rights: "drwxr-xr-x",
+          rights: "drwxr-xr-x", // TODO
           size: stat.size,
           date: dateformat.dateToString(stat.mtime),
           type: stat.isDirectory() ? 'dir' : 'file',
@@ -78,36 +78,34 @@ routes.post('/list', function (req, res, next) {
 
 routes.get('/download', function (req, res, next) {
 
-  var filePath = path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.query.path));
+  var filePath = path.join(pathResolver.baseDir(req), req.query.path);
   var fileName = path.basename(filePath);
   var promise;
-  //console.log('filepath',filePath);
+
   promise = fs.statAsync(filePath);
 
   promise = promise.then(function(stat) {
 
     if(!stat.isFile()) {
-      //console.log("Cannot access file " + filePath + " (or is no file)");
       throw new Error("Cannot access file " + filePath + " (or is no file)");
     }
 
     var mimeType = mime.lookup(filePath);
-    //console.log('mimetype: ' + mimeType);
+    console.log('mimetype: ' + mimeType);
 
-    res.setHeader('Content-disposition', 'attachment; filename=' + encodeURIComponent(fileName));
+    res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
     res.setHeader('Content-type', mimeType);
 
     var filestream = fs.createReadStream(filePath);
     filestream.pipe(res);
   });
 
-  promise = promise.catch(function(err,stats) {
+  promise = promise.catch(function(err) {
     res.status(500);
     res.send({
       "result": {
         "success": false,
-        "error": JSON.stringify(err),
-        "stats": stats
+        "error": err
       }
     });
   });
@@ -128,42 +126,19 @@ routes.post('/upload', upload.any(), function (req, res, next) {
 
 routes.post('/remove', upload.any(), function (req, res, next) {
 
-  var filePaths = req.body.items.map(function(filePath){
-    return path.join(pathResolver.baseDir(req), pathResolver.pathGuard(filePath));
-  });
-
-  var i = filePaths.length;
-  filePaths.forEach(function (path){
-    fs.unlink(path,function (err){
-      i -= 1;
-      if (err){
-        res.status(500);
-        res.send({
-          "result": {
-            "success": false,
-            "error": err
-          }
-        });
-      } else if (i === 0){
-        res.send({
-          "result": {
-            "success": true,
-            "error": null
-          }
-        });
-      }
-    });
-  });
-  // var promise = fs.unlinkAsync(filePath);
+  for(var i = 0, l = req.body.items.length; i < l; i++){
+    var filePath = path.join(pathResolver.baseDir(req), req.body.items[i]);
+    var promise = fs.unlinkAsync(filePath);
+  }
 
   // promise = promise.then(function() {
-  //   res.status(200);
-  //   res.send({
-  //     "result": {
-  //       "success": true,
-  //       "error": null
-  //     }
-  //   });
+  res.status(200);
+  res.send({
+    "result": {
+      "success": true,
+      "error": null
+    }
+  });
   // });
 
   // promise = promise.catch(function(err) {
@@ -176,13 +151,13 @@ routes.post('/remove', upload.any(), function (req, res, next) {
   //   });
   // });
 
-  // return promise;
+  return promise;
 });
 
 routes.post('/createFolder', upload.any(), function (req, res, next) {
 
-  var folderPath = path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.body.path, req.body.name));
-  //console.log(folderPath);
+  var folderPath = path.join(pathResolver.baseDir(req), req.body.newPath);
+  console.log(folderPath);
   var promise = fs.mkdirAsync(folderPath, 0o777);
 
   promise = promise.then(function() {
@@ -210,8 +185,8 @@ routes.post('/createFolder', upload.any(), function (req, res, next) {
 
 routes.post('/rename', function (req, res, next) {
 
-  var oldPath = path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.body.item));
-  var newPath = path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.body.newItemPath));
+  var oldPath = path.join(pathResolver.baseDir(req), req.body.item);
+  var newPath = path.join(pathResolver.baseDir(req), req.body.newItemPath);
 
   var promise = fs.renameAsync(oldPath, newPath);
 
@@ -240,8 +215,8 @@ routes.post('/rename', function (req, res, next) {
 
 routes.post('/copy', function (req, res, next) {
 
-  var oldPath = path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.body.path));
-  var newPath = path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.body.newPath));
+  var oldPath = path.join(pathResolver.baseDir(req), req.body.path);
+  var newPath = path.join(pathResolver.baseDir(req), req.body.newPath);
 
   var promise = fs.copyAsync(oldPath, newPath);
 
@@ -268,124 +243,4 @@ routes.post('/copy', function (req, res, next) {
   return promise;
 });
 
-routes.post('/getContent', function (req, res, next) {
-  res.status(200);
-  var filePath = path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.body.item));
-  var promise = fs.readFileAsync(filePath,'utf8');
-  //console.log(req);
-  promise = promise.then(function (resolve){
-    //console.log(resolve);
-    res.status(200);
-    res.send({
-      "result": resolve
-    });
-  });
-
-  promise=promise.catch(function (err){
-    res.status(500);
-    res.send({
-      "result": {
-        "success": false,
-        "error": err
-      }
-    });
-  });
-
-  return promise;
-});
-
-routes.post('/edit', function (req, res, next) {
-  var filePath = path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.body.item));
-
-  var promise;
-
-  if (req.body.content){
-    promise = fs.writeFileAsync(filePath, req.content , 'utf8');
-  } else {
-    res.status(400);
-    return res.send({
-      "result":{
-        "success":"false",
-        "error": "bad request",
-      }
-    });
-  }
-
-  promise = promise.then(function (resolve){
-    res.status(200);
-    res.send({
-      "result":{
-        "success": true,
-        "error": null
-      }
-    });
-  });
-
-  promise = promise.catch(function (err){
-    res.status(500);
-    res.send({
-      "result": {
-        "success": false,
-        "error": err
-      }
-    });
-  });
-});
-
-routes.post('/move', function (req, res, next) {
-
-  var targets = req.body.items.map(function (filePath){
-    return path.join(pathResolver.baseDir(req), pathResolver.pathGuard(filePath));
-  });
-  var newPath = path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.body.newPath));
-  console.log(newPath);
-  var i = targets.length;
-  targets.forEach(function (target){
-    fs.rename(target,path.join(newPath,path.basename(target)),function (err){
-      i -= 1;
-      if (err){
-        res.status(500);
-        res.send({
-          "result": {
-            "success": false,
-            "error": err
-          }
-        });
-      } else if (i === 0){
-        res.send({
-          "result": {
-            "success": true,
-            "error": null
-          }
-        });
-      }
-    });
-  });
-  // var oldPath = path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.body.item));
-  // var newPath = path.join(pathResolver.baseDir(req), pathResolver.pathGuard(req.body.newItemPath));
-
-  // var promise = fs.renameAsync(oldPath, newPath);
-
-  // promise = promise.then(function() {
-  //   res.status(200);
-  //   res.send({
-  //     "result": {
-  //       "success": true,
-  //       "error": null
-  //     }
-  //   });
-  // });
-
-  // promise = promise.catch(function(err) {
-  //   res.status(500);
-  //   res.send({
-  //     "result": {
-  //       "success": false,
-  //       "error": err
-  //     }
-  //   });
-  // });
-
-  // return promise;
-});
 module.exports = routes;
